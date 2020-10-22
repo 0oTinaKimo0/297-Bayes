419d98c457baab9a98249a3549bd0b59e788da17
0oTinaKimo0 <0oTinaKimo0@users.noreply.github.com>
2020-10-22 14:13:10 -0400

    Skeleton programs

--- NBEDA.py
+++ NBEDA.py
@@ -7,6 +7,12 @@ data = pd.read_csv('adult.csv', header=None)
 data.columns = ['age', 'class', 'fnlwgt', 'edu', 'edunum', 'marital', 'occu', 'relation', 'race', 'sex', 'gain', 'loss', 'hpw', 'country', 'income']
 features = ['age', 'class', 'fnlwgt', 'edunum', 'marital', 'occu', 'relation', 'race', 'sex', 'gain', 'loss', 'hpw', 'country']
 numerical = ['age', 'fnlwgt', 'edunum', 'gain', 'loss', 'hpw']
-print('Class labels:', np.unique(data['edunum']))
+print('Class labels:', np.unique(data['class']))
+print('Class labels:', np.unique(data['marital']))
+print('Class labels:', np.unique(data['occu']))
+print('Class labels:', np.unique(data['relation']))
+print('Class labels:', np.unique(data['race']))
+print('Class labels:', np.unique(data['sex']))
 print('Class labels:', np.unique(data['country']))
+
 print(data.head(20))
