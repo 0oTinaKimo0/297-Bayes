28cf1c74d8e63be582ddb3edda99d8d696ec6faa
0oTinaKimo0 <0oTinaKimo0@users.noreply.github.com>
2020-10-23 01:34:21 -0400

    Handled all features except occupation and work class

--- NBEDA.py
+++ NBEDA.py
@@ -1,21 +1,37 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
+# Load the data and define column names
 data = pd.read_csv('adult.csv', header=None)
 data.columns = ['age', 'work', 'fnlwgt', 'edu', 'edunum', 'marital', 'occu', 'relation', 'race', 'sex', 'gain', 'loss', 'hpw', 'country', 'income']
-features = ['age', 'work', 'fnlwgt', 'edunum', 'marital', 'occu', 'relation', 'race', 'sex', 'gain', 'loss', 'hpw', 'country']
-numerical = ['age', 'fnlwgt', 'edunum', 'gain', 'loss', 'hpw']
+
+# Decide whether to drop NA or to perform imputation
 data.replace(' ?', np.NaN, inplace=True)
 num_rows = len(data.index)
 print("Number of rows: " + str(num_rows))
 data.dropna(inplace=True)
 print("Number of valid rows without missing values: " + str(len(data.index)))
 print("Percentage dropped: {}%". format(100*(1-len(data.index)/num_rows)))
-print('Class labels:', np.unique(data['work']))
-print('Class labels:', np.unique(data['occu']))
-print('Class labels:', np.unique(data['marital']))
-print('Class labels:', np.unique(data['relation']))
-print('Class labels:', np.unique(data['race']))
-print('Class labels:', np.unique(data['country']))
+
+# Try feature selection via dimensionality reduction
+features = ['age', 'work', 'fnlwgt', 'edunum', 'marital', 'occu', 'relation', 'race', 'sex', 'gain', 'loss', 'hpw', 'country']
+numerical = ['age', 'fnlwgt', 'edunum', 'gain', 'loss', 'hpw']
+print('workclass - class labels:', np.unique(data['work']))
+print('occupation - class labels:', np.unique(data['occu']))
+print('marital - class labels:', np.unique(data['marital']))
+print('relationship - class labels:', np.unique(data['relation']))
+print('race - class labels:', np.unique(data['race']))
+print('country - class labels:', np.unique(data['country']))
+plt.hist(data['work'])
+plt.title('Work classes distribution')
+plt.show()
+plt.hist(data['occu'])
+plt.title('Occupation distribution')
+plt.show()
+print(data['country'].describe())
+
+# Describe the distribution of the target classes
 print(data['income'].describe())
-print(data[['work', 'occu', 'marital', 'relation', 'country']].head(20))
+data['income'].replace(' <=50K', 0, inplace=True)
+data['income'].replace(' >50K', 1, inplace=True)
--- NaiveBayes.py
+++ NaiveBayes.py
@@ -9,24 +9,32 @@ from sklearn.metrics import confusion_matrix
 # Load the adult income data from the csv file using pandas
 data = pd.read_csv('adult.csv', header=None)
 data.columns = ['age', 'work', 'fnlwgt', 'edu', 'edunum', 'marital', 'occu', 'relation', 'race', 'sex', 'gain', 'loss', 'hpw', 'country', 'income']
-features = ['age', 'work', 'fnlwgt', 'edunum', 'marital', 'occu', 'relation', 'race', 'sex', 'gain', 'loss', 'hpw', 'country']
-numerical = ['age', 'fnlwgt', 'edunum', 'gain', 'loss', 'hpw']
+features_raw = ['age', 'work', 'fnlwgt', 'edunum', 'marital', 'occu', 'relation', 'race', 'sex', 'gain', 'loss', 'hpw', 'country']
+features = ['age', 'fnlwgt', 'edunum', 'marital', 'relation', 'sex', 'gain', 'loss', 'hpw', 'country']
 data.replace(' ?', np.NaN, inplace=True)
 data.dropna(inplace=True)
 
+# Data preprocessing
 data['income'].replace(' <=50K', 0, inplace=True)
 data['income'].replace(' >50K', 1, inplace=True)
 data['sex'].replace(' Female', 0, inplace=True)
 data['sex'].replace(' Male', 1, inplace=True)
-Y = data['income']
-X = data[numerical]
-print('Using features: ' + str(numerical))
+data['country'].replace(' United-States', 1, inplace=True)
+data['country'].where(data['country'] == 1, 0, inplace=True)
+data['marital'].replace(dict.fromkeys([' Married-AF-spouse', ' Married-civ-spouse', ' Married-spouse-absent'], 1), inplace=True)
+data['marital'].where(data['marital'] == 1, 0, inplace=True)
+data['relation'].replace(' Own-child', 1, inplace=True)
+data['relation'].where(data['relation'] == 1, 0, inplace=True)
 
-# Define the Naive Bayes model
+Y = data['income']
+X = data[features]
+print('Using features: ' + str(features))
+
+# Define the Naive Bayes model - Gaussian
 naiveBayesModel = GaussianNB()
 
 # Split training and testing data
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, stratify=Y)
 
 # Test options and evaluation metric
 scoring = 'accuracy'
@@ -39,6 +47,7 @@ pred_naiveBayesModel = naiveBayesModel.predict(X_test)
 
 # Get metrics for this specific split of train-test data
 print(classification_report(Y_test, pred_naiveBayesModel))
+print(confusion_matrix(Y_test, pred_naiveBayesModel))
 
 # Model Performance
 # Set performance parameters
